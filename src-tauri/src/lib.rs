d2407cb30c097937e241d11b22e0da77b57f2896
Yuhan Liu <againsturb123@gmail.com>
2026-05-26 00:15:17 +0800

    Fixing Android Build Error

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -157,6 +157,7 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    #[allow(unused_mut)]
     let mut builder = tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
